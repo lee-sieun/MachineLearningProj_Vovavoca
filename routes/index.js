159f7b9e25ce6cc07bf7040c5eab0067a85d7515
이시은 <skvkffish@ajou.ac.kr>
2019-10-11 15:40:26 +0900

    data IN

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,26 @@ const router = express.Router();
 const algorithm = require('../src/Ngram');
 
 
+var fs = require('fs');
+
+
+var meanArr= new Array();
+var wordArr= new Array();
+
+fs.readFile('dataset_2.txt','utf-8', function(err, data){
+    var str=data.split('\t');
+    console.log(str.length);
+
+    for(var i=0; i<str.length; i++){
+        wordArr[i]= str[i];
+        console.log(wordArr[i++]);
+        meanArr[i]= str[i];
+        console.log(meanArr[i]);
+    }
+});
+
+
+
 // Main Page
 router.get('/', (req, res) => {
     res.send(true)
