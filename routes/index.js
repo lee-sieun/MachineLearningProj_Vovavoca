39052271662a67773ff4e2db8a6036e627d88b99
이시은 <skvkffish@ajou.ac.kr>
2019-10-11 16:33:43 +0900

    fs_setting

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const algorithm = require('../src/Ngram');
-
-
-var fs = require('fs');
-
+const fs = require('fs');
 
 var meanArr= new Array();
 var wordArr= new Array();
+var strstr= new Array();
+fs.readFile('../dataset/dataset_2.txt','utf-8', function(err, data){
+    str=data.split('\n');
 
-fs.readFile('dataset_2.txt','utf-8', function(err, data){
-    var str=data.split('\t');
-    console.log(str.length);
-
-    for(var i=0; i<str.length; i++){
-        wordArr[i]= str[i];
-        console.log(wordArr[i++]);
-        meanArr[i]= str[i];
-        console.log(meanArr[i]);
+    for(var i =0; i< str.length; i++){
+        strstr.push(str[i].split('\t'));
+        wordArr.push(strstr[i][0]);
+        meanArr.push(strstr[i][1]);
+        //console.log(strstr[i]);
     }
+
+    console.log(wordArr)
 });
 
 
@@ -28,11 +26,18 @@ router.get('/', (req, res) => {
     res.send(true)
 });
 
-const al = algorithm();
-al.KVC("arab", ["ahrab", "arrab", "aaab"], 2)
 
 router.get('/setdataset', (req, res) => {
+    fs.readFile('dataset_2.txt','utf-8', function(err, data){
+        str=data.split('\n');
 
+        for(var i =0; i< str.length; i++){
+            strstr.push(str[i].split('\t'));
+            wordArr.push(strstr[i][0]);
+            meanArr.push(strstr[i][1]);
+            //console.log(strstr[i]);
+        }
+    });
 });
 
 
